f958a21b732c35717ea261e4ef489d5d88708009
zfw <zfw1226@gmail.com>
2019-07-09 22:39:35 +0800

    support single agent env

--- environment.py
+++ environment.py
@@ -14,8 +14,8 @@ def create_env(env_id, args):
         import gym_unrealcv
     env = gym.make(env_id)
     # config observation pre-processing
-    if args.normalize is True:
-        env = NormalizedObs(env)
+    if args.single:
+        env = listspace(env)
     if args.rescale is True:
         env = Rescale(env, args)  # rescale, inv
     if 'img' in args.obs and '2D' not in env_id:
@@ -31,17 +31,13 @@ class Rescale(gym.Wrapper):
         super(Rescale, self).__init__(env)
         self.new_maxd = 1.0
         self.new_mind = -1.0
-        if type(env.observation_space) == list:
-            self.mx_d = 255.0
-            self.mn_d = 0.0
-            shape = env.observation_space[0].shape
-            self.num_agents = len(self.observation_space)
-            self.observation_space = [Box(self.new_mind, self.new_maxd, shape) for i in range(self.num_agents)]
-        else:
-            self.mx_d = env.observation_space.high
-            self.mn_d = env.observation_space.low
-            shape = env.observation_space.shape
-            self.observation_space = Box(self.new_mind, self.new_maxd, shape)
+
+        self.mx_d = 255.0
+        self.mn_d = 0.0
+        shape = env.observation_space[0].shape
+        self.num_agents = len(self.observation_space)
+        self.observation_space = [Box(self.new_mind, self.new_maxd, shape) for i in range(self.num_agents)]
+
         self.obs_range = self.mx_d - self.mn_d
         self.args = args
         self.inv_img = self.choose_rand_seed() and self.args.inv
@@ -83,12 +79,8 @@ class UnrealPreprocess(gym.ObservationWrapper):
         self.crop = args.crop
         self.input_size = args.input_size
 
-        if type(self.observation_space) == list:
-            obs_shape = self.observation_space[0].shape
-            num_agnets = len(self.observation_space)
-        else:
-            obs_shape = self.observation_space.shape
-            num_agnets = None
+        obs_shape = self.observation_space[0].shape
+        num_agnets = len(self.observation_space)
         self.channel = obs_shape[2]
         if abs(obs_shape[0] - self.input_size) + abs(obs_shape[1] - self.input_size) == 0:
             self.resize = False
@@ -96,11 +88,9 @@ class UnrealPreprocess(gym.ObservationWrapper):
             self.resize = True
         if self.gray is True:
             self.channel = 1
-        if num_agnets is None:
-            self.observation_space = Box(-1.0, 1.0, [self.channel, self.input_size, self.input_size], dtype=np.uint8)
-        else:
-            self.observation_space = [Box(-1.0, 1.0, [self.channel, self.input_size, self.input_size],
-                                          dtype=np.uint8) for i in range(num_agnets)]
+
+        self.observation_space = [Box(-1.0, 1.0, [self.channel, self.input_size, self.input_size],
+                                      dtype=np.uint8) for i in range(num_agnets)]
 
     def process_frame_ue(self, frame, size=80):
 
@@ -158,21 +148,20 @@ class frame_stack(gym.Wrapper):
         return np.array(ob)
 
 
-class NormalizedObs(gym.ObservationWrapper):
-    def __init__(self, env=None):
-        gym.ObservationWrapper.__init__(self, env)
-        self.state_mean = 0
-        self.state_std = 0
-        self.alpha = 0.9999
-        self.num_steps = 0
+class listspace(gym.Wrapper):
+    def __init__(self, env):
+        super(listspace, self).__init__(env)
+        if type(self.observation_space) == list:
+            self.num_agents = len(self.observation_space)
+        else:
+            self.observation_space = [self.observation_space]
+            self.action_space = [self.action_space]
+            self.num_agents = 1
 
-    def observation(self, observation):
-        self.num_steps += 1
-        self.state_mean = self.state_mean * self.alpha + \
-            observation.mean() * (1 - self.alpha)
-        self.state_std = self.state_std * self.alpha + \
-            observation.std() * (1 - self.alpha)
+    def reset(self):
+        ob = self.env.reset()
+        return [ob]
 
-        unbiased_mean = self.state_mean / (1 - pow(self.alpha, self.num_steps))
-        unbiased_std = self.state_std / (1 - pow(self.alpha, self.num_steps))
-        return (observation - unbiased_mean) / (unbiased_std + 1e-8)
+    def step(self, action):
+        ob, rew, done, info = self.env.step(action)
+        return [ob], [rew], done, info
