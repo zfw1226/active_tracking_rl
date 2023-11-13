067687360a927494486f1a0322f36fadd6e6b7b4
zfw1226@gmail.com <zfw1226@gmail.com>
2023-11-13 18:47:43 +0800

    close env while keyboardInterrupted

--- test.py
+++ test.py
@@ -51,83 +51,86 @@ def test(args, shared_model, train_modes, n_iters):
     seed = args.seed
     last_iter = 0
     iter_th = args.init_step
-    while True:
-        reward_sum = np.zeros(2)
-        len_sum = 0
-        for i_episode in range(args.test_eps):
-            player.model.load_state_dict(shared_model.state_dict())
-            player.env.seed(seed)
-            # seed += 1
-            player.reset()
-            reward_sum_ep = np.zeros(player.num_agents)
-            fps_counter = 0
-            t0 = time.time()
-            count_eps += 1
-            while True:
-                if args.render:
-                    if 'Unreal' in args.env:
-                        cv2_show(env, False)
-                    else:
-                        env.render()
-                player.action_test()
-                fps_counter += 1
-                reward_sum_ep += player.reward
-                if player.done:
-                    reward_sum += reward_sum_ep[:2]
-                    len_sum += player.eps_len
-                    fps = fps_counter / (time.time()-t0)
-                    n_iter = 0
-                    for n in n_iters:
-                        n_iter += n
-
-                    for rank in range(len(n_iters)):
-                        if n_iter < args.init_step:
-                            train_modes[rank] = 0
-                        elif args.train_mode == 2 and n_iter - last_iter > iter_th:
-                            train_modes[rank] = 1 - train_modes[rank]
-                            last_iter = n_iter
-                            iter_th = args.init_step if train_modes[rank]==0 else args.adv_step
+    try:
+        while True:
+            reward_sum = np.zeros(2)
+            len_sum = 0
+            for i_episode in range(args.test_eps):
+                player.model.load_state_dict(shared_model.state_dict())
+                player.env.seed(seed)
+                # seed += 1
+                player.reset()
+                reward_sum_ep = np.zeros(player.num_agents)
+                fps_counter = 0
+                t0 = time.time()
+                count_eps += 1
+                while True:
+                    if args.render:
+                        if 'Unreal' in args.env:
+                            cv2_show(env, False)
                         else:
-                            train_modes[rank] = args.train_mode
+                            env.render()
+                    player.action_test()
+                    fps_counter += 1
+                    reward_sum_ep += player.reward
+                    if player.done:
+                        reward_sum += reward_sum_ep[:2]
+                        len_sum += player.eps_len
+                        fps = fps_counter / (time.time()-t0)
+                        n_iter = 0
+                        for n in n_iters:
+                            n_iter += n
 
-                    for i, r_i in enumerate(reward_sum_ep):
-                        writer.add_scalar('test/reward'+str(i), r_i, n_iter)
+                        for rank in range(len(n_iters)):
+                            if n_iter < args.init_step:
+                                train_modes[rank] = 0
+                            elif args.train_mode == 2 and n_iter - last_iter > iter_th:
+                                train_modes[rank] = 1 - train_modes[rank]
+                                last_iter = n_iter
+                                iter_th = args.init_step if train_modes[rank]==0 else args.adv_step
+                            else:
+                                train_modes[rank] = args.train_mode
 
-                    writer.add_scalar('test/fps', fps, n_iter)
-                    writer.add_scalar('test/eps_len', player.eps_len, n_iter)
-                    break
+                        for i, r_i in enumerate(reward_sum_ep):
+                            writer.add_scalar('test/reward'+str(i), r_i, n_iter)
 
-        ave_reward_sum = reward_sum/args.test_eps
-        len_mean = len_sum/args.test_eps
-        reward_step = reward_sum / len_sum
-        log['{}_log'.format(args.env)].info(
-            "Time {0}, ave eps reward {1}, ave eps length {2}, reward step {3}".
-            format(
-                time.strftime("%Hh %Mm %Ss",
-                              time.gmtime(time.time() - start_time)),
-                ave_reward_sum, len_mean, reward_step))
+                        writer.add_scalar('test/fps', fps, n_iter)
+                        writer.add_scalar('test/eps_len', player.eps_len, n_iter)
+                        break
 
-        # save model
-        if ave_reward_sum[0] >= max_score:
-            print('Save best!')
-            max_score = ave_reward_sum[0]
-            model_dir = os.path.join(args.log_dir, 'all-best-{0}.dat'.format(n_iter))
-            tracker_model_dir = os.path.join(args.log_dir, 'tracker-best.dat'.format(n_iter))
-            target_model_dir = os.path.join(args.log_dir, 'target-best.dat'.format(n_iter))
-        else:
-            model_dir = os.path.join(args.log_dir, 'all-new.dat'.format(args.env))
-            tracker_model_dir = os.path.join(args.log_dir, 'tracker-new.dat')
-            target_model_dir = os.path.join(args.log_dir, 'target-new.dat')
+            ave_reward_sum = reward_sum/args.test_eps
+            len_mean = len_sum/args.test_eps
+            reward_step = reward_sum / len_sum
+            log['{}_log'.format(args.env)].info(
+                "Time {0}, ave eps reward {1}, ave eps length {2}, reward step {3}".
+                format(
+                    time.strftime("%Hh %Mm %Ss",
+                                  time.gmtime(time.time() - start_time)),
+                    ave_reward_sum, len_mean, reward_step))
 
-        torch.save(player.model.state_dict(), model_dir)
-        if args.split:
-            torch.save(player.model.player0.state_dict(), tracker_model_dir)
-            if not args.single:
-                torch.save(player.model.player1.state_dict(), target_model_dir)
+            # save model
+            if ave_reward_sum[0] >= max_score:
+                print('Save best!')
+                max_score = ave_reward_sum[0]
+                model_dir = os.path.join(args.log_dir, 'all-best-{0}.dat'.format(n_iter))
+                tracker_model_dir = os.path.join(args.log_dir, 'tracker-best.dat'.format(n_iter))
+                target_model_dir = os.path.join(args.log_dir, 'target-best.dat'.format(n_iter))
+            else:
+                model_dir = os.path.join(args.log_dir, 'all-new.dat'.format(args.env))
+                tracker_model_dir = os.path.join(args.log_dir, 'tracker-new.dat')
+                target_model_dir = os.path.join(args.log_dir, 'target-new.dat')
 
-        time.sleep(args.sleep_time)
-        if n_iter > args.max_step:
-            env.close()
-            for id in range(0, args.workers):
-                train_modes[id] = -100
-            break
+            torch.save(player.model.state_dict(), model_dir)
+            if args.split:
+                torch.save(player.model.player0.state_dict(), tracker_model_dir)
+                if not args.single:
+                    torch.save(player.model.player1.state_dict(), target_model_dir)
+
+            time.sleep(args.sleep_time)
+            if n_iter > args.max_step:
+                env.close()
+                for id in range(0, args.workers):
+                    train_modes[id] = -100
+                break
+    except KeyboardInterrupt:
+        env.close()
--- train.py
+++ train.py
@@ -65,46 +65,49 @@ def train(rank, args, shared_model, optimizer, train_modes, n_iters, env=None):
     ave_reward = np.zeros(2)
     ave_reward_longterm = np.zeros(2)
     count_eps = 0
-    while True:
-        # sys to the shared model
-        player.model.load_state_dict(shared_model.state_dict())
+    try:
+        while True:
+            # sys to the shared model
+            player.model.load_state_dict(shared_model.state_dict())
 
-        if player.done:
-            player.reset()
-            reward_sum = torch.zeros(player.num_agents).to(device)
-            reward_sum_org = np.zeros(player.num_agents)
-            count_eps += 1
-
-        player.update_rnn_hiden()
-        t0 = time.time()
-        for i in range(args.num_steps):
-            player.action_train()
-            reward_sum += player.reward
-            reward_sum_org += player.reward_org
             if player.done:
-                for j, r_i in enumerate(reward_sum):
-                    writer.add_scalar('train/reward_' + str(j), r_i, player.n_steps)
+                player.reset()
+                reward_sum = torch.zeros(player.num_agents).to(device)
+                reward_sum_org = np.zeros(player.num_agents)
+                count_eps += 1
+
+            player.update_rnn_hiden()
+            t0 = time.time()
+            for i in range(args.num_steps):
+                player.action_train()
+                reward_sum += player.reward
+                reward_sum_org += player.reward_org
+                if player.done:
+                    for j, r_i in enumerate(reward_sum):
+                        writer.add_scalar('train/reward_' + str(j), r_i, player.n_steps)
+                    break
+            fps = i / (time.time() - t0)
+
+            # cfg training mode
+            # 0: tracker 1: target -1:joint all
+            training_mode = train_modes[rank]
+
+            policy_loss, value_loss, entropies, pred_loss = player.optimize(params, optimizer, shared_model, training_mode, device_share)
+
+            for i in range(min(player.num_agents, 3)):
+                writer.add_scalar('train/policy_loss_'+str(i), policy_loss[i].mean(), player.n_steps)
+                writer.add_scalar('train/value_loss_'+str(i), value_loss[i], player.n_steps)
+                writer.add_scalar('train/entropies'+str(i), entropies[i].mean(), player.n_steps)
+            writer.add_scalar('train/pred_R_loss', pred_loss, player.n_steps)
+            writer.add_scalar('train/ave_reward', ave_reward[0] - ave_reward_longterm[0], player.n_steps)
+            writer.add_scalar('train/mode', training_mode, player.n_steps)
+            writer.add_scalar('train/fps', fps, player.n_steps)
+
+            n_iter += 1
+            n_iters[rank] = n_iter
+
+            if train_modes[rank] == -100:
                 break
-        fps = i / (time.time() - t0)
-
-        # cfg training mode
-        # 0: tracker 1: target -1:joint all
-        training_mode = train_modes[rank]
-
-        policy_loss, value_loss, entropies, pred_loss = player.optimize(params, optimizer, shared_model, training_mode, device_share)
-
-        for i in range(min(player.num_agents, 3)):
-            writer.add_scalar('train/policy_loss_'+str(i), policy_loss[i].mean(), player.n_steps)
-            writer.add_scalar('train/value_loss_'+str(i), value_loss[i], player.n_steps)
-            writer.add_scalar('train/entropies'+str(i), entropies[i].mean(), player.n_steps)
-        writer.add_scalar('train/pred_R_loss', pred_loss, player.n_steps)
-        writer.add_scalar('train/ave_reward', ave_reward[0] - ave_reward_longterm[0], player.n_steps)
-        writer.add_scalar('train/mode', training_mode, player.n_steps)
-        writer.add_scalar('train/fps', fps, player.n_steps)
-
-        n_iter += 1
-        n_iters[rank] = n_iter
-
-        if train_modes[rank] == -100:
-            env.close()
-            break
+        env.close()
+    except KeyboardInterrupt:
+        env.close()
